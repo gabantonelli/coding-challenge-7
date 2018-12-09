81c07f1c045f98dbaf5643f45589dcc613402e17
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2018-12-09 10:28:22 +0100

    added functionality to check for answer, and included everything in a IIFE to make everything private

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
-// constructor function to create new questions
-var Question = function(question, arrAnswers, correctAnswer) {
-  this.question = question;
-  this.answers = arrAnswers;
-  this.correct = correctAnswer;
-};
+(function() {
+  // constructor function to create new questions
+  var Question = function(question, arrAnswers, correctAnswer) {
+    this.question = question;
+    this.answers = arrAnswers;
+    this.correct = correctAnswer;
+  };
 
-// add method to display the question
-Question.prototype = {
-  askQuestion: function() {
-    // log the question
-    console.log(this.question);
-    // log all the possible answers
-    for (var i = 0; i < this.answers.length; i++) {
-      console.log("[" + i + "]" + " " + this.answers[i]);
+  // declare a variable to host user's answer
+  var usersAnswer;
+
+  // add method to display the question
+  Question.prototype = {
+    askQuestion: function() {
+      // log the question
+      console.log(this.question);
+      // log all the possible answers
+      for (var i = 0; i < this.answers.length; i++) {
+        console.log("[" + i + "]" + " " + this.answers[i]);
+      }
+      // prompt the user for the answer
+      usersAnswer = prompt(
+        "Please select the correct answer (just type the number)"
+      );
+    },
+    checkAnswer: function() {
+      if (usersAnswer == this.correct) {
+        console.log("Correct!");
+      } else {
+        console.log("Please try again, you can do better");
+      }
     }
-    // prompt the user for the answer
-    prompt("Please select the correct answer (just type the number)");
-  }
-};
+  };
 
-// create an empty array for storing questions
-var questions = [];
+  // create an empty array for storing questions
+  var questions = [];
 
-// create a few questions (Question instances)
-questions[0] = new Question(
-  "How is Javascript?",
-  ["Hard", "Boring", "Cool"],
-  2
-);
-questions[1] = new Question(
-  "How useful is this quiz?",
-  [
-    "Not useful at all",
-    "Useful only for the programmer to practice",
-    "Very useful"
-  ],
-  1
-);
-questions[2] = new Question(
-  "What is the best browser?",
-  ["MS Edge", "Firefox", "Chrome"],
-  2
-);
+  // create a few questions (Question instances)
+  questions[0] = new Question(
+    "How is Javascript?",
+    ["Hard", "Boring", "Cool"],
+    2
+  );
+  questions[1] = new Question(
+    "How useful is this quiz?",
+    [
+      "Not useful at all",
+      "Useful only for the programmer to practice",
+      "Very useful"
+    ],
+    1
+  );
+  questions[2] = new Question(
+    "What is the best browser?",
+    ["MS Edge", "Firefox", "Chrome"],
+    2
+  );
 
-// pick a random number of question
-var randomQuestion = Math.floor(Math.random() * 3);
+  // pick a random number of question
+  var randomQuestion = Math.floor(Math.random() * 3);
 
-questions[randomQuestion].askQuestion();
+  questions[randomQuestion].askQuestion();
+  questions[randomQuestion].checkAnswer();
+})();
